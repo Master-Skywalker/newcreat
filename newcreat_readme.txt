5ced1c500f9b84daf7ca8e87288adc7604a35143
Master-Skywalker <alin918513@163.com>
2018-06-11 15:31:10 +0800

    all change in stage commit

--- newcreat_readme.txt
+++ newcreat_readme.txt
@@ -1,2 +1,3 @@
 this is a readme text,
 what we want to do is to make the world a better place!
+i am back hhahahhh 
---
+++ 冒泡.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void sort(int *p,int n){
+	int i,j,t;
+	for(i=0;i<n-1;i++){
+		for(j=0;j<n-1-i;j++){
+			if(*(p+j)>*(p+j+1)){
+				t=*(p+j);
+				*(p+j)=*(p+j+1);
+				*(p+j+1)=t;
+			}
+		}
+	}
+	printf("排序后的数组是：\n");
+	for(i=0;i<n;i++)
+		printf("%-3d",*(p+i));
+	printf("\n");
+	return; 
+}
+  
+  
+int main(){
+	int i,j,n,a[20];
+	int*p;
+	p=a;
+	printf("请输入数组个数n:\n");
+	scanf("%d",&n);
+	printf("请输入数组元素:\n");
+	for(i=0;i<n;i++)
+		scanf("%3d",&a[i]);
+	sort(p,n);
+	return 0;
+}
